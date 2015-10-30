6976f11e9661a7707507fcf74e3e98e7776db654
Oliver Davies <oliver@oliverdavies.uk>
2015-10-30 08:58:57 +0000

    Added Compass files

---
+++ config.rb
@@ -0,0 +1,12 @@
+sass_dir = "sass"
+css_dir = "source/css"
+images_dir = "source/assets/images"
+generated_images_dir = images_dir + "/generated"
+javascripts_dir = "source/js"
+output_style = :compressed
+relative_assets = true
+line_comments = false
+
+require 'sass-globbing'
+require 'bootstrap-sass'
+require 'breakpoint'
